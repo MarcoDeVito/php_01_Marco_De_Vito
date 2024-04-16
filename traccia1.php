b226095a105870d18ae3d7f8d959d12c14452aeb
Marco De Vito <marco.devito93@gmail.com>
2024-04-16 18:48:47 +0200

    mi ero dimenticato le costanti -.-'

--- traccia1.php
+++ traccia1.php
@@ -13,3 +13,18 @@ var_dump($str);
 echo "\n";
 var_dump($isBolean);
 echo "\n";
+
+const INTERO= 1;
+const VIRGOLA_MOBILE=1.1;
+const STR="questa è una stringa?";
+const ISBOLEAN=true;
+
+echo "\nle costanti\n";
+var_dump(INTERO);
+echo "\n";
+var_dump(VIRGOLA_MOBILE);
+echo "\n";
+var_dump(STR);
+echo "\n";
+var_dump(ISBOLEAN);
+echo "\n";

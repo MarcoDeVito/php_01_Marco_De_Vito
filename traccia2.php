8b0acffcfd520a6c28b04d8b960d6f9dfc77dc33
Marco De Vito <marco.devito93@gmail.com>
2024-04-16 20:14:18 +0200

    messo una variabile nella traccia 2 per provare/ripassare la string interpolation

--- traccia2.php
+++ traccia2.php
@@ -10,4 +10,6 @@ $text7 = 'bevuto';
 $text8 = "tutto.";
 
 // “Marco hai sete? Perché hai bevuto tutto“
-echo $text1 . " " . $text2 . " " . $text3 . $text4 . " " . $text5 . " " . $text6 . " " . $text7 . " " . $text8;
+$frase="$text1 $text2 $text3$text4 $text5 $text6 $text7 $text8";
+
+echo $frase;

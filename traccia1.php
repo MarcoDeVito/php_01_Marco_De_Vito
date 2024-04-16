3821ba95bb080e6a3b7ef4ef9179635573bfa5ad
Marco De Vito <marco.devito93@gmail.com>
2024-04-16 19:14:44 +0200

    intentato meglio il codice di tutte e 3 le traccie

--- traccia1.php
+++ traccia1.php
@@ -1,9 +1,9 @@
 <?php
 
-$intero= 1;
-$virgolaMobile=1.1;
-$str="questa è una stringa?";
-$isBolean=true;
+$intero = 1;
+$virgolaMobile = 1.1;
+$str = "questa è una stringa?";
+$isBolean = true;
 
 var_dump($intero);
 echo "\n";
@@ -14,10 +14,10 @@ echo "\n";
 var_dump($isBolean);
 echo "\n";
 
-const INTERO= 1;
-const VIRGOLA_MOBILE=1.1;
-const STR="questa è una stringa?";
-const ISBOLEAN=true;
+const INTERO = 1;
+const VIRGOLA_MOBILE = 1.1;
+const STR = "questa è una stringa?";
+const ISBOLEAN = true;
 
 echo "\nle costanti\n";
 var_dump(INTERO);

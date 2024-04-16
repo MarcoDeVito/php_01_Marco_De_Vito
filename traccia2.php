aaaf0c647ea5db27742aea5c53767f1622853e7d
Marco De Vito <marco.devito93@gmail.com>
2024-04-16 18:40:47 +0200

    TRACCIA 2 SVOLTA

---
+++ traccia2.php
@@ -0,0 +1,13 @@
+<?php
+
+$text1 = "Marco";
+$text2 = "hai";
+$text3 = "sete";
+$text4 = "?";
+$text5 = "Perché";
+$text6 = $text2;
+$text7 = 'bevuto';
+$text8 = "tutto.";
+
+// “Marco hai sete? Perché hai bevuto tutto“
+echo $text1." ".$text2." ".$text3.$text4." ".$text5." ".$text6." ".$text7." ".$text8;
